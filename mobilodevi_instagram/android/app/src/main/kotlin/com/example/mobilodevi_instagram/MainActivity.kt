159bea3ae20c0194592bb40bf9737cb8159720a5
deepString <berkaybr.turk@gmail.com>
2023-11-26 20:51:21 +0300

    odevleri yukledim

---
+++ mobilodevi_instagram/android/app/src/main/kotlin/com/example/mobilodevi_instagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobilodevi_instagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobilodevi_youtube/android/app/src/main/kotlin/com/example/mobilodevi_youtube/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobilodevi_youtube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
